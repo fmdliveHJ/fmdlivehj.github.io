295694768c9faf406a9000ad472514fb38da3636
hyeonjin <gdalove@naver.com>
2025-02-22 15:30:40 +0900

    Feat: git pages 추가 설정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   app: {
-    baseURL: '/', // GitHub Pages 루트에서 동작할 경우 `/` 설정
+    baseURL: '/my-blog/',
   },
 });
