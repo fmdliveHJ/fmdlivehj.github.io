b418f82c55bf4eb4133bd78c390774161c89b978
hyeonjin <gdalove@naver.com>
2025-03-04 10:00:45 +0900

    fix: 파일 재설정

---
+++ pages/blog/[slug].vue
@@ -0,0 +1,14 @@
+<script setup>
+const route = useRoute();
+const { data: article } = await useAsyncData(() =>
+  queryContent('blog', route.params.slug).findOne()
+);
+</script>
+
+<template>
+  <article v-if="article">
+    <h1>{{ article.title }}</h1>
+    <p>{{ article.date }}</p>
+    <ContentRenderer :value="article" />
+  </article>
+</template>
